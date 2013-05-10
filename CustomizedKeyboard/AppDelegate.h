e6c73d4b79590218e5ba25eef4f2332a4f5a13af
Minjie Xu <Minjie.Xu@ehi.com>
2013-05-10 16:22:05 -0500

    Initial Commit

---
+++ CustomizedKeyboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomizedKeyboard
+//
+//  Created by Minjie Xu on 5/10/13.
+//  Copyright (c) 2013 Minjie Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomizedKeyboard/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  CustomizedKeyboard
+//
+//  Created by Minjie Xu on 5/10/13.
+//  Copyright (c) 2013 Minjie Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
